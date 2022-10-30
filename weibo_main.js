c3c1c06c42dff532e42b32f924058a41fe08e08c
LieP9 <simohara@outlook.com>
2022-10-30 16:23:59 +0900

    Update weibo_main.js

--- weibo_main.js
+++ weibo_main.js
@@ -18,10 +18,10 @@ const mainConfig = storeMainConfig ? JSON.parse(storeMainConfig) : {
 	modifyMenus: true,			//编辑上下文菜单
 	removeRelateItem: false,	//评论区相关内容
 	removeRecommendItem: true,	//评论区推荐内容
-	removeRewardItem: false,	//微博详情页打赏模块
+	removeRewardItem: true,	//微博详情页打赏模块
 
 	removeLiveMedia: true,		//首页顶部直播
-	removeNextVideo: false,					//关闭自动播放下一个视频
+	removeNextVideo: true,					//关闭自动播放下一个视频
 
 	removePinedTrending: true,		//删除热搜列表置顶条目
 
@@ -29,7 +29,7 @@ const mainConfig = storeMainConfig ? JSON.parse(storeMainConfig) : {
 	removeInterestTopic: false,				//超话：可能感兴趣的超话 + 好友关注
 	removeInterestUser: false,				//用户页：可能感兴趣的人
 
-	removeLvZhou: false,					//绿洲模块
+	removeLvZhou: true,					//绿洲模块
 
 	profileSkin1: null,						//用户页：自定义图标1
 	profileSkin2: null,						//用户页：自定义图标2
@@ -50,19 +50,19 @@ const itemMenusConfig = storeItemMenusConfig ? JSON.parse(storeItemMenusConfig) 
 	mblog_menus_delete:true,				//删除
 	mblog_menus_edit:true,				//编辑
 	mblog_menus_edit_history:true,		//编辑记录
-	mblog_menus_edit_video:true,			//编辑视频
+	mblog_menus_edit_video:false,			//编辑视频
 	mblog_menus_sticking:true,			//置顶
-	mblog_menus_open_reward:true,			//赞赏
+	mblog_menus_open_reward:false,			//赞赏
 	mblog_menus_novelty:false,			//新鲜事投稿
 	mblog_menus_favorite:true,			//收藏
-	mblog_menus_promote:true,				//推广
+	mblog_menus_promote:false,				//推广
 	mblog_menus_modify_visible:true,		//设置分享范围
 	mblog_menus_copy_url:true,			//复制链接
 	mblog_menus_follow:true,				//关注
-	mblog_menus_video_feedback:true,		//播放反馈
+	mblog_menus_video_feedback:false,		//播放反馈
 	mblog_menus_shield:true,				//屏蔽
-	mblog_menus_report:true,				//投诉
-	mblog_menus_apeal:true,				//申诉
+	mblog_menus_report:false,				//投诉
+	mblog_menus_apeal:false,				//申诉
 	mblog_menus_home:true					//返回首页
 }
 
